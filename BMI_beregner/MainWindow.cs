2a999e649f1b5f9523850d26bfb9886097472ca2
kubilayc414 <kubilayc414@gmail.com>
2019-12-02 00:03:03 +0300

    Initial check-in of module BMI_beregner

---
+++ BMI_beregner/MainWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void on_clicked(object sender, EventArgs e)
+    {
+        string vaegtAsaString = textBox_vaegt.Text;
+        string hoejdeAsaString = textBox_hoejde.Text;
+
+        bool isVaegtANumber = double.TryParse(vaegtAsaString, out double vaegt);
+        bool isHoejdeANumber = double.TryParse(hoejdeAsaString, out double hoejde);
+
+        if (isHoejdeANumber == true && isVaegtANumber == true)
+        {
+            double BMI = vaegt / Math.Pow(hoejde / 100.0, 2);
+            textBox_bmi.Text = BMI.ToString();
+        }
+    }
+}
